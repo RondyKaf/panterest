464d68f5e9ea01f71fb8db702535705505536d19
rondykaf <rondykfm@gmail.com>
2023-07-28 00:53:39 +0100

    config pin edit

---
+++ .history/src/Form/PinType_20230727234821.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Entity\Pin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PinType extends AbstractType{
+    public function buildForm(FormBuilderInterface $bluider,array $option){
+        $bluider
+            ->add('title')
+            ->add('description')
+            
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver){
+        $resolver->setDefaults(
+            ['data_class'=> Pin::class]
+        );
+    }
+}
