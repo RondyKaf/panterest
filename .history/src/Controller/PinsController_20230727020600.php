402d175b1c7b2e14824e02377f49c7d925ae4386
rondykaf <rondykfm@gmail.com>
2023-07-27 02:28:37 +0100

    add show page and links between pages

---
+++ .history/src/Controller/PinsController_20230727020600.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(PinRepository $pinRepository): Response
+    {
+        $pins = $pinRepository->findAll(); 
+        return $this->render('pins/index.html.twig',compact('pins'));
+    }
+
+    #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_show',methods:["GET"])]
+    public function show(Pin $pin): Response
+    {
+        dd($pin);
+    }
+}
